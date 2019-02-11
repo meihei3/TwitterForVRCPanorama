e34a18a6d67ef5c914d5518448f72123a8cbaa92
yameholo <ame.aroid.xz5@gmail.com>
2019-02-11 17:41:00 +0900

    Add search function

--- twitter_api.py
+++ twitter_api.py
@@ -19,6 +19,11 @@ def get_tl_id(cnt=10):
     return [tw.id_str for tw in api.home_timeline(count=cnt)]
 
 
+def get_searched_id(trg: str, cnt=10):
+    return [tw.id_str for tw in api.search(q="%s -filter:retweets" % trg, lang='ja',
+                                         result_type='recent', count=cnt)]
+
+
 def retweet(tweet_id: str):
     return api.retweet(str(tweet_id))
 
@@ -30,4 +35,4 @@ def favorite(tweet_id: str):
 if __name__ == '__main__':
     # print(retweet("1094863090003304449"))
     # print(favorite("1094863090003304449"))
-    print(tweet_from_id("1094863090003304449").favorited)
+    pass
